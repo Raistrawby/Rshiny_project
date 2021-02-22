3b5b4e0b671a6279f2950712c9799e9d12f33045
Marie Lahaye <marie76.lahaye@gmail.com>
2021-02-22 15:53:56 +0100

    add default input file

--- server.R
+++ server.R
@@ -5,10 +5,13 @@ library(shiny)
 shinyServer(function(input, output) {
     geneExpression <- reactive({
         file <- input$input
-        ext <- tools::file_ext(input$input$datapath)
-        req(file)
-        validate(need(input$input, ext == "csv", "Please upload a csv file"))
-        data = read.csv(file$datapath, header = input$header)
+        if(is.null(file)) {
+            data = read.csv("data/res_DE2.csv", header = input$header)
+        } else {
+            ext <- tools::file_ext(input$input$datapath)
+            validate(need(ext == "csv", "Please upload a csv file"))
+            data = read.csv(file$datapath, header = input$header)
+        }
     })
     
     output$contents <-
