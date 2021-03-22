40a923182ff5fc770e46f43028ab3d25ebe8f2ac
emma.corre <correemma@yahoo.com>
2021-03-22 16:05:46 +0100

    faut revoir le script go mais grosso merdo c est ca

--- src/UI/go.R
+++ src/UI/go.R
@@ -4,3 +4,38 @@ goUI <- function() {
     mainPanel(dataTableOutput("goContent"))
   ))
 }
+####
+
+d <- read.csv("data/res_DE2.csv")
+geneList <- d[,3] # 
+names(geneList) <- as.character(d[,1])
+geneList <- sort(geneList, decreasing = TRUE)
+data(geneList, package="DOSE")
+head(geneList)
+gene <- names(geneList)[abs(geneList) > 2]
+head(gene)
+
+library(clusterProfiler)
+data(geneList, package="DOSE")
+gene <- names(geneList)[abs(geneList) > 2]
+gene.df <- bitr(gene, fromType = "ENTREZID",
+                toType = c("ENSEMBL", "SYMBOL"),
+                OrgDb = org.Hs.eg.db)
+head(gene.df)
+
+ggo <- groupGO(gene     = gene,
+               OrgDb    = org.Hs.eg.db,
+               ont      = "CC",
+               level    = 3,
+               readable = TRUE)
+
+head(ggo)
+
+ego3 <- gseGO(geneList     = geneList,
+              OrgDb        = org.Hs.eg.db,
+              ont          = "CC",
+              nPerm        = 1000,
+              minGSSize    = 100,
+              maxGSSize    = 500,
+              pvalueCutoff = 0.05,
+              verbose      = FALSE)
