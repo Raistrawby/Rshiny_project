b7e01cfbcc85e63c798417a937a54b1403f2eb6b
emma.corre <correemma@yahoo.com>
2021-03-31 14:58:46 +0200

    ajout des espece sur input MAIS fonctionne pas par server

--- src/UI/input.R
+++ src/UI/input.R
@@ -4,11 +4,11 @@ inputUI <- function() {
       fileInput("input", "Choose CSV File", accept = c(".csv"), placeholder = "exemple.csv"),
       checkboxInput("exemple", "Exemple data", FALSE),
       selectInput("select", label = h3("Select box"), 
-                  choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
-                  selected = 1),
+                  choices = list("Anophele" = 1, "Arabidopsis" = 2, "Bovine" = 3, "Worm" = 4,"Canine" = 5,"Fly" = 6, "ZebraFish" = 7, "Ecoli K12" = 8, "Ecoli Sakai" = 9, "Chicken" = 10, "Human" = 11, "Mouse" = 11,
+                                 "Rhesus" = 12, "Myxococcus xanthus DK 1622" =13 , "Malaria" =14, "Chimp" =15, "Rat" =16, "Yeast" = 17, "Pig" = 19, "Xenopus" = 20), selected = 1),
       
       hr(),
-      fluidRow(column(3, verbatimTextOutput("value")))
+      fluidRow(column(3, verbatimTextOutput("value"))),
     ),
     mainPanel(dataTableOutput("contents"))
   ))
