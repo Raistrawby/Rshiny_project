90bc6665fe4cf5aa4121360c430594ff98df22fd
maneamln <meslin.manea@gmail.com>
2021-05-26 20:22:32 +0200

    Org choice and orgDB install

--- src/server/go/GO.R
+++ src/server/go/GO.R
@@ -1,26 +1,91 @@
 source("./src/server/GSEA_SEA.R")
 
 library(clusterProfiler)
-library(org.Hs.eg.db)
 library(enrichplot)
 library(ggplot2)
 library(ggupset)
 
+# OrgDb ##############################################################
+
+org_to_db <- function(organism) {
+  organisms <-
+    c(
+      "aga",
+      "ath",
+      "bta",
+      "cel",
+      "cfa",
+      "dme",
+      "dre",
+      "eco",
+      "ecs",
+      "gga",
+      "hsa",
+      "mmu",
+      "mcc",
+      "mxa",
+      "pfa",
+      "ptr",
+      "rno",
+      "sce",
+      "ssc",
+      "xla"
+    )
+  orgdbs <-
+    c(
+      "org.Ag.eg.db",
+      "org.At.tair.db",
+      " org.Bt.eg.db",
+      "org.Ce.eg.db",
+      "org.Cf.eg.db",
+      "org.Dm.eg.db",
+      "org.Dr.eg.db",
+      "org.EcK12.eg.db",
+      "org.EcSakai.eg.db",
+      "org.Gg.eg.db",
+      "org.Hs.eg.db",
+      "org.Mm.eg.db",
+      " org.Mmu.eg.db",
+      "org.Mxanthus.db",
+      "org.Pf.plasmo.db",
+      "org.Pt.eg.db",
+      " org.Rn.eg.db",
+      "org.Sc.sgd.db",
+      "org.Ss.eg.db",
+      "org.Xl.eg.db"
+    )
+  org <- grep(organism, organisms)
+  db <- orgdbs[org]
+  return(db)
+}
+
+install_orgDb <- function(OrgDb) {
+  if (!requireNamespace(OrgDb, quietly = TRUE)) {
+    #install.packages(OrgDb)
+    BiocManager::install(OrgDb)
+    library(OrgDb, character.only = TRUE)
+  }
+  else{
+    library(OrgDb, character.only = TRUE)
+  }
+}
+
+
 # GSEA ###############################################################
-gse_analysis <- function(data, method, ontology_gse){
+gse_analysis <- function(data, method, ontology_gse, Orgdb){
   gse <- gseGO(geneList=data$GSEA, 
                ont = ontology_gse, 
                keyType = "ENTREZID", 
                pvalueCutoff = 1,
-               OrgDb = org.Hs.eg.db,
+               OrgDb = Orgdb,
                pAdjustMethod=method)
   return(gse)
 }
 
 # SEA ################################################################
-sea_analysis <- function(data, method, ontology_sea){
+sea_analysis <- function(data, method, ontology_sea, Orgdb){
   go_enrich <- enrichGO(gene = names(data$SEA),
-                        OrgDb = org.Hs.eg.db, 
+                        OrgDb = Orgdb, 
                         keyType = "ENTREZID",
                         ont = ontology_sea,
                         pvalueCutoff = 1, 
